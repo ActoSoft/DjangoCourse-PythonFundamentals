269783638974347ac59bfb111b5df8966e182f24
Martín Melo <martin.melo.dev.97@gmail.com>
2020-06-08 21:06:21 -0500

    Retos tuplas y listas

---
+++ join.py
@@ -0,0 +1,7 @@
+names = []
+selection = "S"
+while selection == "S":
+    name = input('Ingresa un nombre: ')
+    names.append(name)
+    selection = input('Deseas ingresar otro nombre?: ')
+print(','.join(names))
