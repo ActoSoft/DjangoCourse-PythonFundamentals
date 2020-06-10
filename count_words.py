eaea603e2ca964c0744eba4c40046710c5e77bc0
Martín Melo <martin.melo.dev.97@gmail.com>
2020-06-09 20:56:29 -0500

    Reto final dicts

---
+++ count_words.py
@@ -0,0 +1,20 @@
+# El objetivo del programa es contar cuantas veces una palabra está presente en un texto
+# 1.- Separa el texto, guardando cada palabra como un elemento de una lista
+# 2.- Itera la lista y guarda cada palabra como un elemento de un diccionario, donde su valor
+# va a ser la cantidad de veces que aparece esa palabra
+
+
+
+texto = 'Este texto tiene algunas palabras Este es otro texto con algunas palabras diferentes'
+
+# ¿Cómo verificar si ya existe una clave en un diccionario?
+
+dicc = {
+    'clave1': 1,
+    'clave2': 2
+}
+
+if 'clave1' in dicc: # True
+if 'clave3' in dicc: # False
+
+
